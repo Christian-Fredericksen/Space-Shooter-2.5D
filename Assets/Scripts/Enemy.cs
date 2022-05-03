2d7e20d4ba2d8ff26873a789bbe418d0cb7b10de
Christian Fredericksen <christian.fredericksen@yahoo.com>
2022-05-03 14:14:57 -0700

    cleaned up enemy behavior code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,22 +7,22 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(0, 8.0f, 0);
+
     }
 
+
+    [SerializeField]
     private float _speed = 6.0f;
 
     // Update is called once per frame
     void Update()
     {
-        // move down 4 meters per second
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
-
-        // reach bottom and respawn at top
+        
         if (transform.position.y <= -7.0f)
         {
-            transform.position = new Vector3(Random.Range(-9.3f, 9.3f), 8.0f, 0);
+            float randomX = Random.Range(-9.3f, 9.3f);
+            transform.position = new Vector3(randomX, 8.0f, 0);
         }
-        // BONUS: respawn at random X position
     }
 }
