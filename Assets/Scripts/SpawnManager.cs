2d0a6c8558edb3cdb6c872e8eba570f01246bad5
Christian Fredericksen <christian.fredericksen@yahoo.com>
2023-05-06 19:19:33 -0700

    added Triple Shot Powerup to Spawn Manager with coroutine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,8 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyPrefab;
     [SerializeField]
+    private GameObject _tripleShotPowerupPrefab;
+    [SerializeField]
     private GameObject _enemyContainer;
 
     private bool _stopSpawning = false;
@@ -14,18 +16,30 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnRoutine());
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
     }
 
-    IEnumerator SpawnRoutine()
+    IEnumerator SpawnEnemyRoutine()
     {
-        Vector3 spawnX = new Vector3(Random.Range(-9.0f, 9.0f), 6.5f, 0);
         while (_stopSpawning == false)
         {
+            Vector3 spawnX = new Vector3(Random.Range(-9.0f, 9.0f), 6.5f, 0);
             GameObject _newEnemy = Instantiate(_enemyPrefab, spawnX , Quaternion.identity);
             _newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 randoX = new Vector3(Random.Range(-9.0f, 9.0f), 6.5f, 0);
+            Instantiate(_tripleShotPowerupPrefab, randoX, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+        
     }
     public void OnPlayerDeath()
     {
